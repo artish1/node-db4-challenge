fe30fec962e1b177b658e004922f8180c3b9112c
Mark Artishuk <markyshuk@gmail.com>
2019-12-12 14:13:40 -0800

    Added getShoppingList data access function

--- RecipeModel.js
+++ RecipeModel.js
@@ -3,9 +3,20 @@ const config = require("./knexfile");
 const db = knex(config.development);
 
 module.exports = {
-  getRecipes
+  getRecipes,
+  getShoppingList,
+  getInstructions
 };
 
 function getRecipes() {
   return db("recipes");
 }
+
+function getShoppingList(recipe_id) {
+  return db("recipe_ingredients as re")
+    .select("re.quantity", "i.name")
+    .join({ i: "ingredients" }, "re.ingredient_id", "=", "i.id")
+    .where("recipe_id", "=", recipe_id);
+}
+
+function getInstructions(recipe_id) {}
