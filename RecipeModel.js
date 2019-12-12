d558ab770d9d0e244cfb64e9ff3689a3346c9c23
Mark Artishuk <markyshuk@gmail.com>
2019-12-12 14:30:28 -0800

    Added getInstructions method to model

--- RecipeModel.js
+++ RecipeModel.js
@@ -19,4 +19,8 @@ function getShoppingList(recipe_id) {
     .where("recipe_id", "=", recipe_id);
 }
 
-function getInstructions(recipe_id) {}
+function getInstructions(recipe_id) {
+  return db("instructions")
+    .where("recipe_id", "=", recipe_id)
+    .orderBy("step_number");
+}
